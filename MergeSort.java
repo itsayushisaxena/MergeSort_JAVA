99890670d5e81c9d86b7124b52430e0e82456594
AyushiSaxena11 <ayushisaxenamtr@gmail.com>
2020-05-09 20:20:43 +0530

    Completed

--- MergeSort.java
+++ MergeSort.java
@@ -2,7 +2,6 @@
    Ayushi Saxena
    181520015
  */
-package mergesort;
 
 import java.util.Arrays;
 
@@ -19,6 +18,45 @@ public class MergeSort{
 
     public void merge(int[] arr, int low, int mid, int high){
         int[] crr = new int[high + 1];
+        int i = low, j = mid+1, k = 0;
+        while (i <= mid && j <= high) {
+            if(arr[i] <= arr[j]) {
+                crr[k] = arr[i];
+                k++;
+                i++;
+            }
+            else{
+                crr[k] = arr[j];
+                k++;
+                j++;
+            }
+        }
+        if(i > mid){
+            while(j <= high){
+                crr[k] = arr[j];
+                k++;
+                j++;
+            }
+        }
+        else{
+            while(i <= mid){
+                crr[k] = arr[i];
+                k++;
+                i++;
+            }
+        }
+        int p = 0;
+        for (int l = low; l <= high; l++){
+            arr[l] = crr[p];
+            p++;
+        }
+    }
 
+    public static void main(String[] args) {
+        int[] arr = {30,65,20,0,-2,76};
+        System.out.println("Unsorted Array-->" + Arrays.toString(arr));
+        MergeSort obj = new MergeSort();
+        obj.mergeSort(arr, 0, arr.length-1);
+        System.out.println("Sorted Array-->" + Arrays.toString(arr));
     }
 }
