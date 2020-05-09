9a3f73e23d05539c546c373bca4d963d7b8711bf
AyushiSaxena11 <ayushisaxenamtr@gmail.com>
2020-05-09 19:58:09 +0530

    Updated MergeSort.java

--- MergeSort.java
+++ MergeSort.java
@@ -1,1 +1,24 @@
-import java.util.*;
+/*
+   Ayushi Saxena
+   181520015
+ */
+package mergesort;
+
+import java.util.Arrays;
+
+public class MergeSort{
+    public void mergeSort(int[] arr, int low, int high){
+        if(low >= high){
+            return;
+        }
+        int mid = (high + low) / 2;
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
+        merge(arr, low, mid, high);
+    }
+
+    public void merge(int[] arr, int low, int mid, int high){
+        int[] crr = new int[high + 1];
+
+    }
+}
